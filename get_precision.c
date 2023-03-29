a0ae1294ff33a3c49feda538f5c0174aa49f6447
Delbante Alebachew Kassa <122866837+HaileAAle@users.noreply.github.com>
2023-03-29 15:11:13 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -3,9 +3,9 @@
 
 /**
 
- * get_precision - Calculates the precision for printing
+ * get_precision - Calculates the precision for printing.
 
- * @format: Formatted string in which to print the arguments
+ * @format: Formatted string in which to print the arguments.
 
  * @i: List of arguments to be printed.
 
